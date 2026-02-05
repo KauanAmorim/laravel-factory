007cde0793381bb13d370ad06f387efdb0d726d3
KauanAmorim <kauanamorim88@gmail.com>
2026-02-05 14:05:40 -0300

    update: formalizing

---
+++ README.md
@@ -0,0 +1,4 @@
+Its just a practice and a way to create a laravel app just using the command ```sh create.app```
+
+If you want to create and push the created app in a remote repository, you can set te remote url at README.md.
+
--- create-app.sh
+++ create-app.sh
@@ -5,4 +5,12 @@ docker push nauakavlis/laravel-factory:latest && \
 docker run -d --name temp-app nauakavlis/laravel-factory:latest && \
 docker cp temp-app:/var/www/app/. . && \
 docker rm -f temp-app && \
-docker run -d -p 8000:8000 --mount type=bind,source="$(pwd)",target=/var/www/app --name laravel nauakavlis/laravel-factory:latest
+docker run -d -p 8000:8000 --mount type=bind,source="$(pwd)",target=/var/www/app --name laravel nauakavlis/laravel-factory:latest && \
+
+echo "Laravel application is set up and running at http://localhost:8000" && \
+
+rm create-app.sh && \
+
+# git remote add origin <github-repo-url>
+# git branch -M main
+# git push -u origin main
